f271b879f38d7a9dca67575a6e191baea207d4ba
Avazkhon <avazkhonnazirov33@gmail.com>
2023-11-23 23:13:23 +0500

    Day 1 has been added

---
+++ Daily_exercises/day_1/ex_4.py
@@ -0,0 +1,13 @@
+# variables
+
+# There are two variables, a and b from input. The output shows the inputs in the reverse order
+a = input(':' )
+b = input(':' )
+
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
