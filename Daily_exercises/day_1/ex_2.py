8987b2c6f4cee158b013e2bb24e68dff6dc77a99
Avazkhon <avazkhonnazirov33@gmail.com>
2023-11-23 23:17:26 +0500

    Day 1 has been added

--- Daily_exercises/day_1/ex_2.py
+++ Daily_exercises/day_1/ex_2.py
@@ -1,6 +1,6 @@
-# takes a 2-digit number and prints the sum of those 2 numbers
+# Fixing the errors
 
-two_digit_number = (input('Enter a 2-digit num: '))
-first_num = int(two_digit_number[0])
-second_num = int(two_digit_number[1])
-print(first_num+second_num)
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
