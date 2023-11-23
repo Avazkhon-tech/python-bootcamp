f04ef01756444cc5bb20ea5b5f142d08ef1ecbc6
Avazkhon <avazkhonnazirov33@gmail.com>
2023-11-23 22:56:21 +0500

    Day 1 added

---
+++ Daily_exercises/day_1/exercise_4.py
@@ -0,0 +1,6 @@
+# input function 
+# takes 2 numbers from the user and shows the product of those 2 numbers
+
+num1 = int(input('Enter the first num: '))
+num2 = int(input('Enter the second num: '))
+print(f"Product: ", num1*num2)
