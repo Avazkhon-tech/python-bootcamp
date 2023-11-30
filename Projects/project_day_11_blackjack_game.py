ca409ec95bdd900dae7592ce7db40d935fabdfb3
Avazkhon <avazkhonnazirov33@gmail.com>
2023-11-30 23:38:22 +0500

    day 11 has been uploaded

---
+++ Projects/project_day_11_blackjack_game.py
@@ -0,0 +1,16 @@
+should_continue = True
+while should_continue:
+    if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower() != 'y':
+        should_continue = False
+
+
+
+
+
+
+
+
+
+
+
+
