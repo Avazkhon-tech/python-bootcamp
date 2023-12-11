d8ab6b10504ebdda16467039827814cbc8322de4
Avazkhon <avazkhonnazirov33@gmail.com>
2023-12-11 23:00:26 +0500

    day 19 has been uploaded

---
+++ Projects/day_19_and_20_snake_game/snake1.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(2, 0, -1):
+            x_new = self.segments[seg_num - 1].xcor()
+            y_new = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x_new, y_new)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+
+ 
