6127f101a62ddc5a8aaefb014ce6912ea4f0dfbc
Avazkhon <avazkhonnazirov33@gmail.com>
2023-12-15 02:30:30 +0500

    day 23 has been uploaded

--- Projects/day_22_pong_game/main.py
+++ Projects/day_22_pong_game/main.py
@@ -1,70 +1,117 @@
 import turtle
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("Pong")
 screen.tracer(0)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-paddle = Turtle(shape='square')
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.setpos(x=350, y=0)
+ball = Ball()
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
+direction = 'move_top_right'
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.xcor() < -320 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # if ball.xcor() == 390:
+    #     turtle.write('User b lost', font=("white", 24, 'normal'))
+    #     game_is_on = False
+    # if ball.xcor() == - 390:
+    #     turtle.write('User a lost', font=("white", 24, 'normal'))
+    #     game_is_on = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 screen.exitonclick()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen.exitonclick()
 
