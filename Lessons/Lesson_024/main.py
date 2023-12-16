f96b973ac4de807075470d3e63f43e6f9c98d77a
Avazkhon <avazkhonnazirov33@gmail.com>
2023-12-16 15:29:21 +0500

    day 24 has been uploaded

---
+++ Lessons/Lesson_024/main.py
@@ -0,0 +1,13 @@
+# reading the file
+with open("") as file:
+    contents = file.read()
+    print(contents)
+
+# # writing into the file
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text. ")
+
+
+
+
+
--- Projects/day_24_snake_game_improved/scoreboard.py
+++ Projects/day_24_snake_game_improved/scoreboard.py
@@ -1,14 +1,15 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
+with open("data.txt", 'r') as file:
+    HIGHSCORE = int(file.read())
 
 
 class Scoreboard(Turtle):
-
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = HIGHSCORE
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -19,14 +20,13 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"Score: {self.score} High score: {self.highscore} ", align=ALIGNMENT, font=FONT)
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
         self.score = 0
+        with open("data.txt", 'w') as file:
+            file.write(f'{self.highscore}')
+            self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
