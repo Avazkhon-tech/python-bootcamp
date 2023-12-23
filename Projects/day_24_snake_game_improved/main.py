ab1ecba1f2e17909692f00707267d4918d04461d
Avazkhon <avazkhonnazirov33@gmail.com>
2023-12-23 22:58:43 +0500

    updated

--- Projects/day_24_snake_game_improved/main.py
+++ Projects/day_24_snake_game_improved/main.py
@@ -19,6 +19,10 @@ screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "W")
+screen.onkey(snake.down, "S")
+screen.onkey(snake.left, "A")
+screen.onkey(snake.right, "D")
 
 game_is_on = True
 while game_is_on:
