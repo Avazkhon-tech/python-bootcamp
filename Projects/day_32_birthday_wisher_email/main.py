63ea4cec843a758364265daad1a97e6d7de6e439
Avazkhon <avazkhonnazirov33@gmail.com>
2024-01-02 23:52:00 +0500

    updated

--- Projects/day_32_birthday_wisher_email/main.py
+++ Projects/day_32_birthday_wisher_email/main.py
@@ -2,9 +2,9 @@
 import datetime as dt
 import pandas
 from random import choice
+import smtplib
 
 letters = ['letter_1.txt', 'letter_2.txt', 'letter_3.txt']
-
 now = dt.datetime.now()
 day_now = now.day
 month_now = now.month
@@ -13,7 +13,13 @@ year_now = now.year
 data = pandas.read_csv('birthdays.csv')
 no_of_people = len(data)
 
+my_email = "a69707776@gmail.com"
+password = "ahll idge dpwo uqhg"
+
+
+
 for i in range(no_of_people):
+    email = data.loc[i]['email']
     name = data.loc[i]['name']
     year = data.loc[i]['year']
     month = data.loc[i]['month']
@@ -23,6 +29,14 @@ for i in range(no_of_people):
         with open(f'letter_templates/{letter}', 'r') as file:
             wish = file.read()
             wish = wish.replace('[NAME]', name)
+
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(my_email, password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=email,
+                                msg=f"Subject: Happy Birthday\n\n"
+                                    f"{wish}")
 
 
 
